f291ca2ba8628dbe749ac88601ba0078d32f04d7
Marcin Gesek <marcin.gesek79@gmail.com>
2021-12-26 21:47:45 +0100

    Separation of the files

---
+++ lib/todoList.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class TodoList extends StatefulWidget {
+  const TodoList({Key? key}) : super(key: key);
+
+  @override
+  _TodoListState createState() => _TodoListState();
+}
+
+class _TodoListState extends State<TodoList> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Todo List"),
+      ),
+    );
+  }
+}
